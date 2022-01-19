b6f26bd680b40667b0ec9506b142dc8f834068c2
sergio.ortiz <sergio.ortiz.fp@iescampanillas.com>
2022-01-19 07:28:00 -0500

    Tests

--- Funciones.py
+++ Funciones.py
@@ -1,7 +1,14 @@
 def esPrimo(numero):
-    primo = 0
-    for i in range(2, numero):
-        resto = numero % i
-        if resto == 0:
-            primo = primo+1
-    return primo == 0
+    try:
+        if numero==True:
+            return False
+    try:
+        primo = 0
+        for i in range(2, numero):
+            resto = numero % i
+            if resto == 0:
+                primo = primo+1
+        return primo == 0
+    except TypeError:
+        return False
+    except 
